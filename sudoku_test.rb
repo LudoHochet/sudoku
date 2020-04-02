e6c49d34df9707792576b3b9cd0bb5384c6cd2c9
Patrice Cadiot <pathibul.r@gmail.com>
2020-04-02 20:25:36 +0200

    1st good solution

--- sudoku_test.rb
+++ sudoku_test.rb
@@ -265,22 +265,67 @@ class SudokuTest < Minitest::Test
             [9, 6, 1, 5, 3, 7, 2, 8, 4],
             [2, 8, 7, 4, 1, 9, 6, 3, 5],
             [3, 4, 5, 2, 8, 6, 1, 7, 9]]
-    filled_grid  = Sudoku.new(grid).fill_all
+    filled_grid = Sudoku.new(grid).fill_all
     assert_equal 'Finished!', Sudoku.done_or_not(filled_grid)
   end
 
-  def test_it_can_fill_three_missing_digit
+  def test_it_can_fill_three_missing_digits
     # skip
-    grid = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
-            [6, 7, 2, 1, 9, 5, 3, 4, 8],
-            [1, 9, 8, 3, 4, 2, 5, 6, 7],
-            [8, 5, 0, 7, 6, 1, 4, 2, 3],
-            [4, 2, 6, 8, 5, 3, 0, 9, 1],
-            [7, 1, 3, 9, 2, 4, 8, 5, 6],
-            [9, 6, 1, 5, 3, 7, 2, 8, 4],
-            [2, 8, 7, 4, 1, 9, 6, 3, 5],
-            [3, 4, 5, 2, 8, 6, 1, 0, 9]]
-    filled_grid  = Sudoku.new(grid).fill_all
+    grid_3 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+              [6, 7, 2, 1, 9, 5, 3, 4, 8],
+              [1, 9, 8, 3, 4, 2, 5, 6, 7],
+              [8, 5, 0, 7, 6, 1, 4, 2, 3],
+              [4, 2, 6, 8, 5, 3, 0, 9, 1],
+              [7, 1, 3, 9, 2, 4, 8, 5, 6],
+              [9, 6, 1, 5, 3, 7, 2, 8, 4],
+              [2, 8, 7, 4, 1, 9, 6, 3, 5],
+              [3, 4, 5, 2, 8, 6, 1, 0, 9]]
+    filled_grid = Sudoku.new(grid_3).fill_all
+    assert_equal 'Finished!', Sudoku.done_or_not(filled_grid)
+  end
+
+  def test_it_can_fill_for_missing_digits
+    # skip
+    grid_4 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+              [6, 7, 2, 1, 9, 5, 3, 4, 8],
+              [1, 9, 0, 3, 4, 2, 5, 6, 7],
+              [8, 5, 0, 7, 6, 1, 4, 2, 3],
+              [4, 2, 6, 8, 5, 3, 0, 9, 1],
+              [7, 1, 3, 9, 2, 4, 8, 5, 6],
+              [9, 6, 1, 5, 3, 7, 2, 8, 4],
+              [2, 8, 7, 4, 1, 9, 6, 3, 5],
+              [3, 4, 5, 2, 8, 6, 1, 0, 9]]
+    filled_grid = Sudoku.new(grid_4).fill_all
+    assert_equal 'Finished!', Sudoku.done_or_not(filled_grid)
+  end
+
+  def test_it_can_fill_six_missing_digits
+    # skip
+    grid_6 = [[0, 3, 4, 6, 7, 8, 9, 1, 2],
+              [6, 7, 0, 1, 9, 5, 3, 4, 8],
+              [1, 9, 8, 3, 4, 2, 5, 6, 7],
+              [8, 5, 0, 7, 6, 1, 4, 2, 3],
+              [4, 2, 6, 8, 5, 3, 0, 9, 1],
+              [7, 1, 3, 9, 2, 4, 8, 5, 6],
+              [9, 6, 1, 5, 3, 7, 2, 8, 4],
+              [2, 8, 0, 4, 1, 9, 6, 3, 5],
+              [3, 4, 5, 2, 8, 6, 1, 0, 9]]
+    filled_grid = Sudoku.new(grid_6).fill_all
+    assert_equal 'Finished!', Sudoku.done_or_not(filled_grid)
+  end
+
+  def test_it_can_fill_ten_missing_digit
+    # skip
+    grid_10 = [[5, 3, 4, 6, 0, 8, 9, 1, 2],
+               [6, 7, 2, 1, 9, 5, 0, 4, 8],
+               [1, 9, 8, 3, 4, 2, 5, 0, 7],
+               [8, 5, 0, 7, 6, 1, 4, 2, 3],
+               [4, 2, 6, 8, 0, 3, 0, 9, 1],
+               [0, 1, 3, 9, 2, 4, 8, 5, 6],
+               [9, 6, 1, 0, 3, 7, 2, 8, 4],
+               [2, 8, 7, 4, 0, 9, 6, 3, 5],
+               [3, 4, 5, 2, 8, 6, 1, 0, 9]]
+    filled_grid = Sudoku.new(grid_10).fill_all
     assert_equal 'Finished!', Sudoku.done_or_not(filled_grid)
   end
 
